3f32e4cf5ad262ba70a31f2fc55569ca71e3c8b0
lwarnb17 <war17001@byui.edu>
2019-02-20 11:58:43 -0700

    Finsished Project

--- GlazerCalculation/MainPage.xaml.cs
+++ GlazerCalculation/MainPage.xaml.cs
@@ -26,5 +26,70 @@ namespace GlazerCalculation
         {
             this.InitializeComponent();
         }
+
+        private void QuantitySlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            var QuantitySlider = sender as Slider;
+
+            var quantity = QuantitySlider.Value;
+            SliderValue.Text = quantity.ToString();
+
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            widthString = WidthValue.Text;
+            width = double.Parse(widthString);
+            heightString = HeightValue.Text;
+            height = double.Parse(heightString);
+
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            LengthValue.Text = woodLength.ToString() + " feet";
+            GlassValue.Text = glassArea.ToString() + " square meters";
+            DateValue.Text = DateTime.Now.ToString("dddd, dd, MMMM, yyyy");
+            
+            }
+
+        private void HeightValue_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            string height = HeightValue.Text.Trim();
+            for (int i = 0; i < height.Length; i++)
+            {
+                if (!char.IsNumber(height[i]))
+                {
+                    MessageBox.Text = "Please enter a vaild number.";
+                    HeightValue.Text = "";
+                    return;
+                }else
+                       {
+                    MessageBox.Text = "";
+                       }
+            }
+           
+        }
+
+        private void WidthValue_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            string width = WidthValue.Text.Trim();
+            for (int i = 0; i < width.Length; i++)
+            {
+                if (!char.IsNumber(width[i]))
+                {
+                    MessageBox.Text = "Please enter a vaild number.";
+                    WidthValue.Text = "";
+                    return;
+                }
+                else
+                {
+                    MessageBox.Text = "";
+                }
+            }
+        }
     }
-}
+    }
+
